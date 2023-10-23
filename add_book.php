aa4d5dc849707c49d7b2b7004edaec5352bec80e
tiffanydawn29 <tskogan@northislandcollege.ca>
2023-10-23 10:55:41 -0700

    Add previous star rating system

--- add_book.php
+++ add_book.php
@@ -17,7 +17,22 @@
             <option value="Yes">Yes</option>
             <option value="No">No</option>
         </select><br>
-
-        </form>
+        If yes, series name: <input type="text" name="series_name"><br>
+        <div class="centered-items">Rating: <br> 
+            <div class="rate">
+              <input type="radio" id="star5" name="rate" value="5" />
+              <label for="star5" title="text">5 stars</label>
+              <input type="radio" id="star4" name="rate" value="4" />
+              <label for="star4" title="text">4 stars</label>
+              <input type="radio" id="star3" name="rate" value="3" />
+              <label for="star3" title="text">3 stars</label>
+              <input type="radio" id="star2" name="rate" value="2" />
+              <label for="star2" title="text">2 stars</label>
+              <input type="radio" id="star1" name="rate" value="1" />
+              <label for="star1" title="text">1 star</label>
+            </div>
+            <br> <input type="submit" value="Add Book">
+        </div>
+    </form>
 </body>
 </html>
