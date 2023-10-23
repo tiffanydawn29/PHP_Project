9df37dc63c7396e4db9403b0cfba364bc0733cb7
tiffanydawn29 <tskogan@northislandcollege.ca>
2023-10-23 10:53:48 -0700

    Add new styled book entry form

--- add_book.php
+++ add_book.php
@@ -7,5 +7,17 @@
 </head>
 <body>
 
+<h1>Add a new Bookish Entry</h1>
+    <form action="process_book.php" method="post">
+        Author:<br> <input type="text" name="author" required><br>
+        Title:<br> <input type="text" name="title" required><br>
+        Genre:<br> <input type="text" name="genre" required><br>
+        Series:<br> 
+        <select name="series" required>
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+        </select><br>
+
+        </form>
 </body>
 </html>
