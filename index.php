a95865ec5adc12abf6f64b0b95abb3b2ede2e907
tiffanydawn29 <tskogan@northislandcollege.ca>
2023-10-23 10:39:52 -0700

    Add html Blog skeleton

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Book Blog</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+
+</head>
+<body>
+    <h1>Welcome to the Bookish blog</h1>
+    <a href="add_book.php">Add a New Book Entry</a>
+
+    <h2>Recent Book Entries:</h2>
+
+<p> Add Book Entry PHP code here </p>
+
+    </body>
+</html>
