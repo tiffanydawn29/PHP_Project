ed5a5ef2698ab22d9e741a063bef187d000bcda4
tiffanydawn29 <tskogan@northislandcollege.ca>
2023-10-23 11:42:52 -0700

    Add header with nav

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
     <link rel="stylesheet" type="text/css" href="styles.css">
 
 </head>
+<header>
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="add_book.php">Add a Book</a></li>
+    </ul>
+    </nav>
+</header>
 <body>
     <h1>Welcome to the Bookish blog</h1>
     <a href="add_book.php">Add a New Book Entry</a>
