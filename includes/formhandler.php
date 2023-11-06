774891313a29568079af4e3cbbf0dec7fafd62ea
tiffanydawn29 <tskogan@northislandcollege.ca>
2023-11-05 16:16:57 -0800

    Create formhandler.php

---
+++ includes/formhandler.php
@@ -0,0 +1,33 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $pwd = $_POST["pwd"];
+    $email = $_POST["email"];
+
+    try {
+        require_once "dbh.php";
+
+        $query = "INSERT INTO users(username, pwd, email) VALUES (:username, :pwd, :email);";
+
+        $stmt = $pdo->prepare($query);
+
+        $stmt->bindParam(":username", $username);
+        $stmt->bindParam(":pwd", $pwd);
+        $stmt->bindParam(":email", $email);
+
+        $stmt->execute();
+
+        $pdo = null;
+        $stmt = null;
+
+        header("location:  ../index.php");
+
+        die();
+    } catch (PDOException $e){
+        die("Query failed: " . $e->getMessage());
+    }
+}
+else {
+    header("location:  ../signup.php");
+}
