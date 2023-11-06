d0f1e16a4978a4984f4052d1c276346dcb988f24
tiffanydawn29 <tskogan@northislandcollege.ca>
2023-11-05 16:17:16 -0800

    Remove space

--- includes/dbh.php
+++ includes/dbh.php
@@ -6,7 +6,7 @@ $dbpassword = "root";
 
 try {
     $pdo = new PDO($dsn, $dbusername, $dbpassword);
-    $pdo->setAttribute(PDO:: ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Connection failed" . $e->getMessage();
 }
