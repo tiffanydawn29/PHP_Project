e8e5659031cf6d509aafd3e25efdddc49ceb3e7a
tiffanydawn29 <tskogan@northislandcollege.ca>
2023-11-27 11:02:29 -0800

    Add PDO connection

--- process_book.php
+++ process_book.php
@@ -4,4 +4,12 @@
     $username = "root";
     $password = "root";
     $dbname = "myphpproject";
+
+    try {
+        // Create a PDO connection
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    
+        // Set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
 ?>
