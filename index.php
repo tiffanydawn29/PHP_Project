3f44cf597ca32193b2c246c0702961d354dba81e
tiffanydawn29 <tskogan@northislandcollege.ca>
2023-11-27 10:58:33 -0800

    Create PDO connection

--- index.php
+++ index.php
@@ -30,6 +30,15 @@
     $username = "root";
     $password = "root";
     $dbname = "myphpproject";
+
+    try {
+      // Create a PDO connection
+      $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+      // Set the PDO error mode to exception
+       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
     ?>
 
     <p>Add Book Entry PHP code here</p>
